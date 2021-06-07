0a51c8e0364d41c6be3487386886191a310a6af8
Krishna Sai <krishna31082001@gmail.com>
2021-06-07 16:09:09 +0530

    Hosting Changes

--- devathon/vendor/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Token, ExtraItems, Transaction
-# Register your models here.
-admin.site.register(Token)
-admin.site.register(ExtraItems)
-admin.site.register(Transaction)
